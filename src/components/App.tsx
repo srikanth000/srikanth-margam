87fb1f0ce128fdb6a10c3062e330a8c5015dc447
srikanth000 <margamsrikanth7@gmail.com>
2021-12-08 22:02:45 +0530

    feat: Init repo

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles.scss';
+
+/* eslint-disable-next-line */
+const App = () => (
+  <div className="wrapper">
+    <h1>Here I am</h1>
+  </div>
+);
+export default App;
